45b362f5feaeba2ed4610af38f7daa0c5accaaa2
Giovani <43485102+giovaniGdy@users.noreply.github.com>
2019-05-21 16:16:40 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
+import authorRoutes from './routes/AuthorRoutes'
+import bookRoutes from './routes/BookRoutes'
+
 const app = express()
 const BASE_URI = '/api/v1'
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(BASE_URI, authorRoutes)
+app.use(BASE_URI, bookRoutes)
 
 app.get('*', (req, res) => {
   res.status(200).send({messsage: 'Welcome to this API'})
